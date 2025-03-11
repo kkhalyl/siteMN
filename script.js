56763a12aae15644869233b10d678890fefee326
khalylk <113326748+kkhalyl@users.noreply.github.com>
2025-03-11 13:52:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 const slider = document.querySelector('.slider-range');
-const overlay = document.querySelector('.slider-overlay');
-const playButton = document.querySelector('#playButton'); // Get the play button
-const video = document.querySelector('#yourVideoElement'); // Replace with your video element selector
-
-slider.addEventListener('input', () => {
-    overlay.style.width = `${slider.value}%`;
-    if (slider.value == slider.max) {
-        slider.classList.add('hidden'); // Hide the slider
-        playButton.classList.remove('hidden'); // Show the play button
-    } else {
-        slider.classList.remove('hidden'); // Show the slider
-        playButton.classList.add('hidden'); // Hide the play button
-    }
-});
-
-playButton.addEventListener('click', () => {
-    video.classList.remove('hidden'); // Mostra o vídeo
-    video.play().catch(error => {
-        console.error('Erro ao reproduzir o vídeo:', error);
-    });
-});
+ const overlay = document.querySelector('.slider-overlay img');
+ const overlay = document.querySelector('.slider-overlay');
+ 
+ slider.addEventListener('input', (e) => {
+     let sliderValue = e.target.value;
+     overlay.style.clipPath = `inset(0 ${100 - sliderValue}% 0 0)`;
+ slider.addEventListener('input', () => {
+     overlay.style.width = `${slider.value}%`;
+ });
