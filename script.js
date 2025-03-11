1008b71917ff9a677ae4f446636ba2c772a897c0
khalylk <113326748+kkhalyl@users.noreply.github.com>
2025-03-11 14:10:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const slider = document.querySelector('.slider-range');
-const overlayImage = document.querySelector('.slider-overlay img');
+const overlay = document.querySelector('.slider-overlay');
 
 slider.addEventListener('input', (e) => {
     let sliderValue = e.target.value;
-    overlayImage.style.clipPath = `inset(0 ${100 - sliderValue}% 0 0)`;
-});
-slider.addEventListener('input', () => {
-    overlayImage.style.width = `${slider.value}%`;
+    overlay.style.width = `${sliderValue}%`;
 });
